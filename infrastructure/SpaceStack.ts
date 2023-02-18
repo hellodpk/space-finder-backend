bf13e1fa0b8812721a65461db78733f9cac3be86
CORP\Deepak.Singh <deepak.singh@powerschool.com>
2023-02-18 16:05:41 +0530

    initial commit

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import {LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway'
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class SpaceStack extends Stack{
 
@@ -22,6 +23,11 @@ export class SpaceStack extends Stack{
             entry:(join(__dirname,'..','services','node-lambda','hello.ts')),
             handler:'handler'
         })
+        const s3ListPolicy=new PolicyStatement();
+        s3ListPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListPolicy.addResources('*');
+        helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
+
             //hello Api lambda integration
             const helloLambdaIntegration=new LambdaIntegration(helloLambdaNodeJs)
             const helloLambdaResource=this.api.root.addResource('hello');
