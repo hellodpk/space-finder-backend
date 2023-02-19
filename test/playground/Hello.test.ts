bf2bcbbd5a4df969dba91a5e18b342e3e009551b
CORP\Deepak.Singh <deepak.singh@powerschool.com>
2023-02-19 17:34:45 +0530

    lambda read with secondary param

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -3,7 +3,8 @@ import { handler } from '../../services/SpacesTable/Read';
 
 const event : APIGatewayProxyEvent = {
     queryStringParameters:{
-        spaceId:'bef1f443-0c5a-4f7d-8093-93e46f04e905'
+        //spaceId:'bef1f443-0c5a-4f7d-8093-93e46f04e905'
+        location:'Oregon'
     }
 } as any;
 
